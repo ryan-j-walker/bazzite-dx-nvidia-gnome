1e986a74f8abb1ace60979a89c50df9e6b4c09d3
Ryan Walker <ryanwalker8484@pm.me>
2026-06-19 10:16:45 -0400

    Adding library for blur my shell rounded corners

--- build_files/theming.sh
+++ build_files/theming.sh
@@ -9,3 +9,6 @@ echo "Installation of MoreWaita icons started"
 git clone https://github.com/somepaulo/MoreWaita.git
 ./MoreWaita/install.sh
 rm -rf ./MoreWaita
+
+echo "Installation of Gnome Round Corner Blur Library"
+curl https://raw.githubusercontent.com/aunetx/blur-my-shell/refs/heads/master/scripts/rounded_blur_build.sh | bash -s -- -i
